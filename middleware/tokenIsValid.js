83b40d3481c866501aa46daaee86c03d494901d3
Strassboom <maddog241996@gmail.com>
2021-12-11 14:49:59 -0500

    Allowed user to change username, password, and email

---
+++ middleware/tokenIsValid.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function tokenIsValid(request, response, next) {
+    if (request.cookies.token) {
+        jwt.verify(request.cookies.token, 'secret', (err, decodedToken) => {
+            if (err) {
+                request.tokenIsValid = false;
+            }
+            else {
+                request.email = decodedToken.email;
+                request.tokenIsValid = true;
+            }
+        });
+    }
+    else {
+        request.tokenIsValid = false;
+    }
+    next();
+}
